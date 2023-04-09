955a9ec0d9d9946530f1f34830e21ba21f05b214
Stephanie Jauch <sjauch21@gmail.com>
2023-04-09 17:31:23 -0500

    added questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [] => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'project_title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information',
+      },
+      {
+        type: 'list',
+        name: 'contributing',
+        message: 'List all contributors',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter tests performed',
+      },
+    ]);
+  };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
